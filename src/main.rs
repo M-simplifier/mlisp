959db0d10c61cde3e47626cbd750c128b94d4eb1
M-simplifier <95624086+M-simplifier@users.noreply.github.com>
2022-09-15 19:54:28 +0900

    Enable Input/Output on terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Error: at read_line().");
+    println!("{buf}");
 }
